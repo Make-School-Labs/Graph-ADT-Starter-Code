eff88bfd9e2e246773594c7a36f975b1a2be36b6
Meredith Murphy <meredithcat@gmail.com>
2021-01-20 19:50:02 -0800

    update main.py

--- main.py
+++ main.py
@@ -30,25 +30,18 @@ if __name__ == '__main__':
     # graph = read_graph_from_file('test_files/graph_small_directed.txt')
 
     # Output the vertices & edges
-    # Print vertices
-    print(f'The vertices are: {graph.get_vertices()} \n')
-
-    # Print edges
-    print('The edges are:')
-    for vertex_obj in graph.get_vertices():
-        for neighbor_obj in vertex_obj.get_neighbors():
-            print(f'({vertex_obj.get_id()} , {neighbor_obj.get_id()})')
+    print(graph)
 
     # Search the graph
-    print('Performing BFS traversal...')
-    graph.bfs_traversal('A')
+    # print('Performing BFS traversal...')
+    # graph.bfs_traversal('A')
 
     # Find shortest path
-    print('Finding shortest path from vertex A to vertex E...')
-    shortest_path = graph.find_shortest_path('A', 'E')
-    print(shortest_path)
+    # print('Finding shortest path from vertex A to vertex E...')
+    # shortest_path = graph.find_shortest_path('A', 'E')
+    # print(shortest_path)
 
     # Find all vertices N distance away
-    print('Finding all vertices distance 2 away...')
-    vertices_2_away = graph.find_vertices_n_away('A', 2)
-    print(vertices_2_away)
+    # print('Finding all vertices distance 2 away...')
+    # vertices_2_away = graph.find_vertices_n_away('A', 2)
+    # print(vertices_2_away)
--- tests/test_graph_traversals.py
+++ tests/test_graph_traversals.py
@@ -1,6 +1,5 @@
 
 import unittest
-from gradescope_utils.autograder_utils.decorators import weight, visibility
 from graphs.graph import Graph
 
 
